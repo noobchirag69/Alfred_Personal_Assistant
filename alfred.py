2cd208f48c3360ee3c7b9c7179df22991b104301
noobchirag69 <chiragchakraborty48@gmail.com>
2022-05-12 16:40:55 +0530

    Added the code

---
+++ alfred.py
@@ -0,0 +1,223 @@
+# Importing necessary modules
+import pyttsx3  # Fpr speaking functionality
+import speech_recognition as sr  # To recognize user's voice
+import datetime  # To know date and time
+import sounddevice as sd  # To remove audio errors
+import wikipedia  # For fetching info from Wikipedia
+import webbrowser as web  # For opening websites
+import os  # For opening desktop files and applications
+import sys  # For cross-platform functionalities
+import subprocess  # To spawn new processes
+import pywhatkit as pw  # For some specific functionalities like google search
+import pyjokes  # For fetching jokes
+import requests # For sending HTTP requests
+import wolframalpha # For geopraphical, computational or GK questions
+from bs4 import BeautifulSoup # For pulling news out of news website
+
+
+# Setting the voice using Pyttsx3 and Espeak
+engine = pyttsx3.init('espeak')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[11].id)
+
+
+# Speak Function using Pyttsx3
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+# Wish Function
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        print("Good Morning!")
+        speak("Good Morning!")
+    elif hour >= 12 and hour < 18:
+        print("Good Afternoon!")
+        speak("Good Afternoon!")
+    else:
+        print("Good Evening!")
+        speak("Good Evening!")
+
+    print("I am Alfred, at your service!")
+    speak("I am Alfred, at your service!")
+
+
+# Function for commanding Alfred using SpeechRecognition
+def takeCommand():
+    listener = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        listener.pause_threshold = 1
+        listener.adjust_for_ambient_noise(source)
+        audio = listener.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = listener.recognize_google(audio, language='en')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Sorry, say that again please!")
+        speak("Sorry, say that again please!")
+        return "None"
+
+    return query
+
+
+# Functionalities of Alfred
+def runAlfred():
+    print("Tell me how can I help you...")
+    speak("Tell me how can I help you.")
+    query = takeCommand().lower()
+
+    # Identity
+    if 'who are you' in query:
+        print("I am Alfred, your personal assistant! I was created by Mr. Chirag Chakraborty in the year 2022. As of now, I am programmed to open a few websites, play music from your device, play songs on YouTube, search on Google, fetch info from Wikipedia, giving you the present time, read out today's news headlines from BBC, answer some geographical as well as computational questions, locate a place in Google Maps and entertain you with one-liner programming jokes.")
+        speak("I am Alfred, your personal assistant! I was created by Mr. Cheerag Chakraborty in the year 2022. As of now, I am programmed to open a few websites, play music from your device, play songs on YouTube, search on Google, fetch info from Wikipedia, giving you the present time, read out today's news headlines from BBC, answer some geographical as well as computational questions, locate a place in Google Maps and entertain you with one-liner programming jokes.")
+
+    elif 'who made you' in query or 'who created you' in query:
+        print("I was created by Mr. Chirag Chakraborty.")
+        speak("I was created by Mr. Cheerag Chakraborty.")
+
+    # Logic for executing tasks based on query
+    # Fetch info from Wikipedia
+    elif 'wikipedia' in query:
+        speak('Searching Wkipedia...')
+        query = query.replace("wikipedia", "")
+        results = wikipedia.summary(query, sentences=2, auto_suggest=False)
+        speak("According to Wikipedia")
+        print(results)
+        speak(results)
+
+    # Browser actions using WebBrowser
+    elif 'open youtube' in query:
+        web.open("youtube.com")
+
+    elif 'open google' in query:
+        web.open("google.com")
+
+    elif 'open whatsapp' in query:
+        web.open("web.whatsapp.com")
+
+    elif 'open reddit' in query:
+        web.open('reddit.com')
+
+    elif 'open spotify' in query:
+        web.open('open.spotify.com')
+
+    elif 'open telegram' in query:
+        web.open('web.telegram.org')
+
+    elif 'open colab' in query:
+        web.open('colab.research.google.com')
+
+    elif 'open mail' in query:
+        web.open('mail.google.com')
+
+    # Locate a place in Google Maps using WebBrowser
+    elif "where is" in query:
+        query = query.replace("where is", "")
+        location = query
+        print("Locating" + location + " " + "in Google Maps")
+        speak("Locating" + location + " " + "in Google Maps")
+        web.open("https://www.google.co.in/maps/place/" + location + "")
+
+    # Playing a particular song on YouTube using PyWhatKit
+    elif 'play song' in query:
+        song = query.replace('play', '')
+        print('Playing' + song)
+        speak('Playing' + song)
+        pw.playonyt(song)
+
+    # Google Search using PyWhatKit
+    elif 'search' in query:
+        strSearch = query.replace('search', '')
+        print('Searching on Google...')
+        speak('Searching on Google!')
+        pw.search(strSearch)
+
+    # OS actions
+    elif 'play music' in query:
+        music_dir = '/home/chirag/Music'
+        songs = os.listdir(music_dir)
+        print(songs)
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, os.path.join(music_dir, songs[0])])
+
+    # Time using DateTime
+    elif 'the time' in query:
+        strTime = datetime.datetime.now().strftime("%I:%M %p")
+        print(f"The time is {strTime}\n")
+        speak(f"The time is {strTime}\n")
+
+    # Jokes using PyJokes
+    elif 'joke' in query:
+        print(pyjokes.get_joke())
+        speak(pyjokes.get_joke())
+
+    # For fetching News using Requests and BeautifulSoup
+    elif 'news' in query:
+        url = 'https://www.bbc.com/news'
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        headlines = soup.find('body').find_all('h3')
+        unwanted = ['BBC World News TV', 'BBC World Service Radio',
+            'News daily newsletter', 'Mobile app', 'Get in touch']
+        
+        print("According to BBC...")
+        speak("According to BBC")
+        
+        for x in list(dict.fromkeys(headlines)):
+            if x.text.strip() not in unwanted:
+                print(x.text.strip())
+                speak(x.text.strip())
+
+    # For questions
+    elif 'ask' in query:
+        print('Please ask your question. I will try my best to answer.')
+        speak('Please ask your question. I will try my best to answer.')
+    
+    # For computational questions
+    elif 'calculate' in query:
+        app_id = "L4YQ8E-PL486JPL4L"
+        client = wolframalpha.Client('L4YQ8E-PL486JPL4L')
+        indx = query.lower().split().index('calculate')
+        query = query.split()[indx + 1:]
+        res = client.query(' '.join(query))
+        answer = next(res.results).text
+        print(answer)
+        speak(answer)
+
+    # For Geographical questions
+    elif 'what is' in query or 'who is' in query:
+        app_id = "L4YQ8E-PL486JPL4L"
+        client = wolframalpha.Client('L4YQ8E-PL486JPL4L')
+        res = client.query(query)
+        answer = next(res.results).text
+
+        try:
+            print(answer)
+            speak(answer)
+        except Exception as e:
+            print("Sorry, No results!")
+            speak("Sorry, no results!")
+
+
+    # Exit
+    elif 'good bye' in query:
+        print("Thank you. Alfred signing off!")
+        speak("Thank you. Alfred signing off!")
+        exit()
+
+# Main Function
+if __name__ == "__main__":
+    clear = lambda: os.system('clear')
+
+    clear() # Function to clear out every command before the execution
+    wishMe() # Wish the user at the beginning
+
+    while True:
+        runAlfred()
